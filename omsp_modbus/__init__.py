e406f2fd85838fd928fc7c05eec7918ab1d87829
Brian McClune <bpmcclune@gmail.com>
2022-08-31 08:25:51 -0400

    Add recv_msg method to Streamer

--- omsp_modbus/__init__.py
+++ omsp_modbus/__init__.py
@@ -1,9 +1,11 @@
 """Defines an OMSP JSON-to-Modbus streaming parser."""
 
+import json
 import socket
 
 from typing import Optional, Tuple
 
+import crcmod.predefined
 
 class Streamer:
     """The OMSP JSON-to-Modbus streamer.
@@ -14,6 +16,15 @@ class Streamer:
             by setting this to ``False``.
     """
 
+    #: The buffer size for receiving data.
+    BUFSIZE = 1024
+
+    #: The OMSP NULL byte.
+    NULL = b'\x00'
+
+    #: The OMSP CRLF bytes.
+    CRLF = b'\r\n'
+
     def __init__(self, address: Tuple[str, int], connect: bool = True):
         """Contructor."""
 
@@ -22,6 +33,12 @@ class Streamer:
 
         #: The current connection for the Streamer.
         self.conn = None
+
+        #: The Streamer buffer.
+        self._buf = b''
+
+        #: The Cyclic Redundancy Check, CRC-16-ANSI (x8005), used by OMSP.
+        self._crc16 = crcmod.predefined.mkPredefinedCrcFun('modbus')
 
         if connect:
             self.connect()
@@ -42,3 +59,43 @@ class Streamer:
             self.conn.close()
 
         self.conn = socket.create_connection(self.address)
+
+    def recv_msg(self) -> dict:
+        """Receives a message on the current connection.
+
+        Returns:
+            The JSON message.
+        """
+
+        # Assert there is a connection before receiving
+        if self.conn is None:
+            raise ValueError(
+                'Streamer is not connected! Use .connect() to start listening.'
+            )
+
+        # Receive in BUFSIZE chunks until NULL byte is encountered
+        msgs = []
+        remainder = b''
+        while True:
+            msg = self.conn.recv(self.BUFSIZE)
+            if self.NULL in msg:
+                msg, remainder = msg.split(self.NULL, 1)
+            msgs.append(msg)
+            if remainder:
+                break
+
+        # Assemble complete message including remainder from prior receives
+        self._buf += b''.join(msgs)
+
+        # Split JSON bytes and checksum
+        jsonb, cksum = self._buf.split(self.CRLF)
+
+        # Assert jsonb matches checksum
+        crc16 = hex(self._crc16(jsonb))
+        if crc16[2:].upper() != cksum.decode():
+
+            # TODO: Improve reporting for this error
+            raise ValueError('CRC-16-ANSI checksum does not match')
+
+        # Return the JSON message
+        return json.loads(jsonb)
